242f281f4502d4bd1ff9c8351a60c9fce54c6057
SalvadorCarnelutti <salvadorcarnelutti@gmail.com>
2023-05-13 21:33:39 -0400

    Removed the blur effect as it was having some unwanted behavour
    Each time the loader was presented and then blurred and un-blurred the navbar would shift up a bit and make both "Logout" and the back button unaccesible

--- SwiftUITraining/Utils/LoadingView.swift
+++ SwiftUITraining/Utils/LoadingView.swift
@@ -7,44 +7,26 @@
 
 import SwiftUI
 
-struct ActivityIndicator: UIViewRepresentable {
-    @Binding var isAnimating: Bool
-    let style: UIActivityIndicatorView.Style
-
-    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
-        return UIActivityIndicatorView(style: style)
-    }
-
-    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
-        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
-    }
-}
-
 struct LoadingView<Content>: View where Content: View {
     @Binding var isShowing: Bool
     var content: () -> Content
 
     var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .center) {
+        ZStack(alignment: .center) {
+            self.content()
+                .disabled(isShowing)
 
-                self.content()
-                    .disabled(isShowing)
-                    .blur(radius: isShowing ? 3 : 0)
-
-                VStack {
-                    Text("Loading...")
-                    ActivityIndicator(isAnimating: .constant(true),
-                                      style: .large)
-                }
-                .frame(width: geometry.size.width / 2,
-                       height: geometry.size.height / 5)
-                .background(Color.secondary.colorInvert())
-                .foregroundColor(Color.primary)
-                .cornerRadius(20)
-                .opacity(isShowing ? 1 : 0)
-
+            VStack {
+                Text("Loading...")
+                    .font(.headline)
+                ProgressView()
             }
+            .frame(width: 200,
+                   height: 160)
+            .background(Color.secondary.colorInvert())
+            .foregroundColor(Color.primary)
+            .cornerRadius(20)
+            .opacity(isShowing ? 1 : 0)
         }
     }
 }
@@ -53,7 +35,7 @@ struct LoadingView_Previews: PreviewProvider {
     static var previews: some View {
         LoadingView(isShowing: .constant(true)) {
             NavigationView {
-                Text("Test to se how it gets blurred when loading")
+                Text("Test to se how it gets when loading")
                     .frame(alignment: .center)
             }
         }
